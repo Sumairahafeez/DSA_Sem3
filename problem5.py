81651440bdd81901c4b64e3e12e365422a15d03f
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-09-08 11:07:30 +0500

    Lab1 Tasks completed

--- problem5.py
+++ problem5.py
@@ -1,9 +1,6 @@
 def StringReverse(str,starting,ending):
     str2 = str[starting:ending]
-    if ending == 0:
-        return str2
-    else:
-        StringReverse(str2,0,ending-1)
+    return str2[::-1] 
 s = input("Enter a string: ")
 starting = int(input("Enter the starting portion: "))
 ending = int(input("Enter the ending portion: "))
