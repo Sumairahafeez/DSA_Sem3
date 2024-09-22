f3b8174fd11bb27c8a2e758a309615617e3c3c24
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-09-22 18:33:07 +0500

    Add files via upload

--- Week 3/part2.py
+++ Week 3/part2.py
@@ -9,9 +9,21 @@ def PlotData(symptomp):
     plt.title(symptomp)
     plt.ylabel(symptomp)
     plt.show()
+#Function That compares data with Test Data
+def TestAndTrain(symptomp):
+    Disease_Name = test[symptomp].values.tolist() 
+    for i in Disease_Name:
+        for j in TestSymptomp:
+            if i == j:
+                print(f'test row matches with the training row')
+            else:
+                print("Result doesnot matches")    
+
 #Main That Gets the Data 
 df = pd.read_csv('Train.csv')
+test = pd.read_csv('Test.csv')
 Symptomps = df.columns[:-1]
+TestSymptomp = test.columns[:-1]
 for i in Symptomps:
     PlotData(i)
-
+TestAndTrain(TestSymptomp)
